121c4ca5db3390c65cd3cdd96c76e60c14cfb256
Markus Harrison <jodaddy15@gmail.com>
2018-11-03 23:23:29 -0700

    sidebar complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,44 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')
-const autoprefixer = require('gulp-autoprefixer')
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
 // const concat = require('gulp-concat')
 // const babel = require('gulp-babel')
 // const watch = require('gulp-watch')
-const browserSync = require('browser-sync')
-const reload = browserSync.reload
-var exec = require('child_process').exec;
+const browserSync = require("browser-sync");
+const reload = browserSync.reload;
+var exec = require("child_process").exec;
 
-gulp.task('default', ['styles', 'webpack', 'browser-sync'], () => {
-  gulp.watch('./assets/sass/**/*', ['styles'])
-  gulp.watch('./assets/js/**/*', ['webpack'])
-  gulp.watch(['./public/**/*', './public/*', '!public/js/**/.#*js', '!public/css/**/.#*css']).on('change', reload)
-})
+gulp.task("default", ["styles", "webpack", "browser-sync"], () => {
+  gulp.watch("./assets/sass/**/*", ["styles"]);
+  gulp.watch("./assets/js/**/*", ["webpack"]);
+  gulp
+    .watch([
+      "./public/**/*",
+      "./public/*",
+      "!public/js/**/.#*js",
+      "!public/css/**/.#*css"
+    ])
+    .on("change", reload);
+});
 
-gulp.task('styles', () => {
-  gulp.src('assets/sass/**/*.scss')
+gulp.task("styles", () => {
+  gulp
+    .src("assets/sass/**/*.scss")
     .pipe(
       sass({
-        outputStyle: 'compressed'
+        outputStyle: "compressed"
+      }).on("error", sass.logError)
+    )
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
       })
-      .on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions']
-    }))
-    .pipe(gulp.dest('./public/css'))
-    .pipe(browserSync.stream())
-})
+    )
+    .pipe(gulp.dest("./public/css"))
+    .pipe(browserSync.stream());
+});
 
-gulp.task('browser-sync', ['styles'], function () {
+gulp.task("browser-sync", ["styles"], function() {
   // THIS IS FOR SITUATIONS WHEN YOU HAVE ANOTHER SERVER RUNNING
   // browserSync.init({
   //   proxy: {
@@ -39,19 +49,19 @@ gulp.task('browser-sync', ['styles'], function () {
   // })
 
   browserSync.init({
-        server: './public',
-        notify: false,
-        open: false //change this to true if you want the broser to open automatically 
-    });
-})
+    server: "./public",
+    notify: false,
+    open: true //change this to true if you want the browser to open automatically
+  });
+});
 
-gulp.task('webpack', (cb) => {
-  exec('webpack', function (err, stdout, stderr) {
-      console.log(stdout);
-      console.log(stderr);
-      cb(err);
-    });
-})
+gulp.task("webpack", cb => {
+  exec("webpack", function(err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
 
 // gulp.task('webpack', shell.task([
 //   'webpack'
